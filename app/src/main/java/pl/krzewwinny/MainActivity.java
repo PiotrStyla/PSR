3df9e612a489d2b8e77196335a4a4b6ccb3fcc66
Piotr Styła <31997939+PiotrStyla@users.noreply.github.com>
2020-06-18 12:06:08 +0200

    + telefon + progressbar +wycofywanie

--- app/src/main/java/pl/krzewwinny/MainActivity.java
+++ app/src/main/java/pl/krzewwinny/MainActivity.java
@@ -1,13 +1,27 @@
 package pl.krzewwinny;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CALL = 1;
+    public ProgressBar progressBar;
 
     private WebView webView;
     @Override
@@ -15,11 +29,71 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        progressBar = (ProgressBar)findViewById(R.id.prg);
+
+        ImageView imageCall = findViewById(R.id.image_call);
+
+        imageCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makePhoneCall();
+            }
+        });
+
         webView = findViewById(R.id.webView);
-        webView.setWebViewClient(new myWebClient());
+
+
+
+
+        webView.setWebViewClient(new myWebClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+                setTitle(view.getTitle());
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE);
+                setTitle("Pobieram dane...");
+
+            }
+        });
+
+
+
+
+
+
+
+
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("https://www.krzewwinny.pl/nowenna-przed-zeslaniem-ducha-swietego/");
 
+    }
+
+    private void makePhoneCall(){
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+        } else {
+            String dial = "tel:" + "+48784345625";
+            startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CALL) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                makePhoneCall();
+            } else {
+                Toast.makeText(this, "Brak zgody na wykonywanie połączeń z tej aplikacji", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     public class myWebClient extends WebViewClient
@@ -44,6 +118,7 @@ public class MainActivity extends AppCompatActivity {
             webView.goBack();
         } else {
             super.onBackPressed();
+            finishAffinity();
         }
     }
 }
